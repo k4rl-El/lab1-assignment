1d202bfcac35d0b332f25e8c193028384301fbe7
unknown <chukwuma1497@gmail.com>
2022-04-02 12:36:02 +0100

    lab 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab1_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my name is Chukwuma Nnaemeka Elvis, I am in the backend development track of Wella schools");
+            Console.WriteLine("I am learning backend development because i have always been interested in how things work under the hood and i love to solve problems");
+        }
+    }
+}
